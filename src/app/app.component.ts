10398c0cc8330920ae0f7bff296e363766ab0556
LebokgangSteve <lebokgang@digilink.africa>
2023-04-21 14:54:41 +0200

    Typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+//lorem.sentence();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    //if(value == this.randomText)
+    this.enteredText = value;
+  }
+  compare(randomLetter: String, enteredLetter: String) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    if (enteredLetter === randomLetter) {
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
 }
